3b388af83a0ced4df42b6c40d3bb8f18adbfe7c2
Auston Stewart <auston@shedbuilt.net>
2018-01-10 02:33:17 -0500

    Updating to 4.14.1

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+# Prevent installation of docs for arpd, which won't be installed
 sed -i /ARPD/d Makefile
-sed -i 's/arpd.8//' man/man8/Makefile
-rm -v doc/arpd.sgml
+rm -fv man/man8/arpd.8
 sed -i 's/m_ipt.o//' tc/Makefile
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} DOCDIR=/usr/share/doc/iproute2-4.12.0 install
+make -j $SHED_NUMJOBS || return 1
+make DESTDIR="$SHED_FAKEROOT" DOCDIR=/usr/share/doc/iproute2-4.14.1 install || return 1
