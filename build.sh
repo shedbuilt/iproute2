ecf4b2a28b08045a7909ef941c33d5e9cfc33d61
Auston Stewart <auston@shedbuilt.net>
2018-02-05 07:02:49 -1000

    Updating to 4.15.0

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 sed -i /ARPD/d Makefile
 rm -fv man/man8/arpd.8
 sed -i 's/m_ipt.o//' tc/Makefile
-make -j $SHED_NUMJOBS || return 1
-make DESTDIR="$SHED_FAKEROOT" DOCDIR=/usr/share/doc/iproute2-4.14.1 install || return 1
+make -j $SHED_NUMJOBS && \
+make DESTDIR="$SHED_FAKEROOT" DOCDIR=/usr/share/doc/iproute2-4.15.0 install
