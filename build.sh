35761d1d9717c78d19218a62468f6f864e67e220
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:24:35 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 sed -i /ARPD/d Makefile
 rm -fv man/man8/arpd.8
 sed -i 's/m_ipt.o//' tc/Makefile
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" DOCDIR=/usr/share/doc/iproute2-4.15.0 install
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" DOCDIR=/usr/share/doc/iproute2-4.15.0 install
