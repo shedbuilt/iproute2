3bae83bad3324e7d73a254cff82df51e51a9423f
Auston Stewart <auston@shedbuilt.net>
2018-08-14 12:10:06 -1000

    Updating to 4.17.0

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
-SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
 # Prevent installation of docs for arpd, which won't be installed
 sed -i /ARPD/d Makefile &&
 rm -fv man/man8/arpd.8 &&
 # Disable building a module that depends on iptables
 sed -i 's/m_ipt.o//' tc/Makefile &&
 make -j $SHED_NUM_JOBS &&
-make DESTDIR="$SHED_FAKE_ROOT" DOCDIR="$SHED_PKG_LOCAL_DOCDIR" install || exit 1
+make DESTDIR="$SHED_FAKE_ROOT" DOCDIR="$SHED_PKG_DOCS_INSTALL_DIR" install || exit 1
 # Optionally remove documentation
 if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
-    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+    rm -rf "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}"
 fi
